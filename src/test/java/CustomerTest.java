0d78e1613261d551113b14c5a12420627e2334e0
Reubin George <george.reubin@gmail.com>
2019-04-16 12:51:25 -0400

    jUnit Commit 4

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -10,14 +10,25 @@ class CustomerTest {
 
     @Test
     void ytdFees() {
+        Bank bank = new Bank("Wells Fargo");
+        Customer customer1 = new Customer(bank,"Cruise","Tom");
+        assertEquals(0.0,customer1.ytdFees());
     }
 
     @Test
     void ytdInterest() {
+        Bank bank = new Bank("Chase");
+        Customer customer1 = new Customer(bank, "Jolie","Angelica");
+        assertEquals(0.0, customer1.ytdInterest());
     }
 
     @Test
     void addSavingsAccount() {
+        Bank bank = new Bank("Bank of America");
+        Customer customer1 = new Customer(bank, "Depp","Johnny");
+        SavingsAccount savingAccounts1 = customer1.addSavingsAccount(300000,
+                "Johnny Depp's savings account");
+        assertEquals(300000, savingAccounts1.getBalance());
     }
 
     @Test
