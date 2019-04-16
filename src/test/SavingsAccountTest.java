485f6e903f298c7c866c643925560973ac9da3e6
jasysak <jsysak@hawkmail.hccfl.edu>
2019-04-16 15:37:26 -0400

    A little more progress...more to come

--- src/test/SavingsAccountTest.java
+++ src/test/SavingsAccountTest.java
@@ -1,19 +1,23 @@
 package test;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertNotNull;
+
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import banking.Account;
 import banking.Bank;
 import banking.Customer;
+import banking.SavingsAccount;
 
 /*
  * BankTest.java 
@@ -28,20 +32,18 @@ import banking.Customer;
  * Erin Hochstetler
  * Jason Sysak
  * 
-*/
-
-class CustomerTest {
-	
+ */
+public class SavingsAccountTest {
 	// will definitely need some Sets of Account and Customer
 	SortedSet<Account> accountsSet = new TreeSet<Account>();
 	SortedSet<Customer> customersSet = new TreeSet<Customer>();
 	// will need a List of Customer, maybe Account too
 	List<Account> accountsArrList = new ArrayList();
 	List<Customer> customersArrList = new ArrayList();
-		
-	// might need a bank object
+	
+	// will need a bank object
 	Bank bank;
-		
+	
 	// will need more than one customer object, and Strings FName/LName to fill
 	// appropriate fields for creating
 	Customer cust1;
@@ -50,6 +52,17 @@ class CustomerTest {
 	Customer cust2;
 	String cust2FName;
 	String cust2LName;
+	double initBalance = 1000.1;
+	SavingsAccount cust1Account1;
+	SavingsAccount cust2Account1;
+	
+	
+	// we might need a Map<Customer, Set<Account>> for cases where a customer has 
+	// many accounts. I'll add this later if it's needed.
+	
+	// from Bank.java
+	private final Map<String, Customer> customersMap = new HashMap<>();
+	
 	
 	@Before
 	public void initializeTests() {
@@ -61,52 +74,46 @@ class CustomerTest {
 		cust2LName = "Greene";
 		cust1 = new Customer(bank, cust1FName, cust1LName);
 		cust2 = new Customer(bank, cust2FName, cust2LName);	
-	}
-	
-	@Test
-	void testCustomer() {
-		
+		cust1Account1 = new SavingsAccount(cust1, initBalance, "Test Account Customer 1");
+		cust2Account1 = new SavingsAccount(cust2, initBalance, "Test Account Customer 2");
 	}
 
 	@Test
-	void testGetCustomerAccounts() {
-		
-		customersSet.add(cust1);
-		assertNotNull(customersSet);
+	public void testDeposit() {
+		double depositAmount = 100.5;
+		cust1Account1.deposit(depositAmount);
+		assertEquals(cust1Account1.getBalance(), (initBalance + depositAmount), 0);
 	}
 
 	@Test
-	void testYtdFees() {
-		// maybe a test for a specific Fees value should be added?
-		assertNotNull(cust1.ytdFees());
+	public void testWithdraw() {
+		double withdrawAmount = 100.5;
+		cust1Account1.withdraw(withdrawAmount);
+		assertEquals(cust1Account1.getBalance(), (initBalance - withdrawAmount), 0);
 	}
 
 	@Test
-	void testYtdInterest() {
-		// maybe a test for a specific Interest value should be added?
-		assertNotNull(cust1.ytdInterest());
+	public void testSavingsAccountCustomerDoubleString() {
+		fail("Not yet implemented");
 	}
 
 	@Test
-	void testAddSavingsAccount() {
-		double initBal = 1000.5;
-		String desc = "Test Account CustomerTest";
-		assertNotNull(cust1.addSavingsAccount(initBal, desc));
+	public void testSavingsAccountCustomerDoubleStringDouble() {
+		fail("Not yet implemented");
 	}
 
 	@Test
-	void testRemoveAccount() {
-		String custID = cust1.getCustomerId();
-		double initBal = 1000.5;
-		String desc = "Test Account CustomerTest";
-		accountsArrList.add(cust1.addSavingsAccount(initBal, desc));
-		cust1.removeAccount(custID);
-		assertNull(accountsArrList);
-		// not sure if the above 3 lines will actually work...
+	public void testAddInterestTransaction() {
+		fail("Not yet implemented");
 	}
 
 	@Test
-	void testGetAccount() {
+	public void testGetDefaultInterestRate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDefaultInterestRate() {
 		fail("Not yet implemented");
 	}
 
